a5168b44556363e020c68c0f8b5ca258ec1a9b14
Ivan Sakhonchik <ivan.sakhonchik@gmail.com>
2019-11-28 10:14:05 +0300

    GmailandMailRu

---
+++ HW5/HW5/PageMailRu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace HW5
+{
+    class PageMailRu
+    {
+        IWebDriver browser = new ChromeDriver();
+        public void InputForMail()
+        {
+
+            browser.Navigate().GoToUrl("https://mail.ru");
+
+            IWebElement login = GetElement(By.Id("mailbox:login"), browser);
+            login.SendKeys("accountforwebdriver" + Keys.Enter);
+
+            IWebElement password = GetElement(By.Id("mailbox:password"), browser);
+            password.SendKeys("Asdfghjk543" + Keys.Enter);
+
+        }
+        //public void SendMessage()
+        //{
+        //    IWebElement buttonWrite = GetElement(By.ClassName("compose-button__txt"), browser);
+        //    buttonWrite.Click();
+        //}
+
+        public void ReadMessage()
+        {
+            IWebElement messageUnread = GetElement(By.XPath("//span[@title='Вася Пупкин <trainingwebdriver@gmail.com>']"),browser);
+            messageUnread.Click();
+        }
+
+        public void Quit()
+        {
+            browser.Quit();
+        }
+
+
+        private IWebElement GetElement(By elementLocator, IWebDriver driver)
+        {
+            return new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementIsVisible(elementLocator));
+        }
+    }
+}
---
+++ HW5/HW5/Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace HW5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PageGmailCom pageGmailCom = new PageGmailCom();
+            pageGmailCom.InputForMail();
+            pageGmailCom.SendMessage();
+            pageGmailCom.Quit();
+
+            PageMailRu pageMailRu = new PageMailRu();
+            pageMailRu.InputForMail();
+            pageMailRu.ReadMessage();
+            pageMailRu.Quit();
+
+        }
+    }
+}
